91032a6defaaa0abb6c13c14fb86cb2557875a1b
ericodess <erickfcaldeira@gmail.com>
2022-01-07 19:03:13 -0300

    Introduced Preview Redirector component

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { default as Button } from './Button';
 export { default as ContactForm } from './ContactForm';
 export { default as Divisor } from './Divisor';
 export { default as Modal } from './Modal';
+export { default as PreviewButton } from './PreviewButton';
