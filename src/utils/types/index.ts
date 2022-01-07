7bb6f02fe5cba9846e575da62d4a601d28fea064
ericodess <erickfcaldeira@gmail.com>
2022-01-07 18:56:37 -0300

    Introduced component extractor service

--- src/utils/types/index.ts
+++ src/utils/types/index.ts
@@ -1,3 +1,15 @@
+/**
+ * @description A method that return a React component.
+ */
 export type ComponentGetter = () => React.ReactNode;
 
+/**
+ * @description A common prop that both accepts a standalone React component
+ * or a method that must be run in other to retrieve a React component.
+ */
 export type ComponentAsProp = React.ReactNode | ComponentGetter;
+
+/**
+ * @description A common component onclick event handler type.
+ */
+export type ComponentClickEvent = (event: React.MouseEvent) => void;
