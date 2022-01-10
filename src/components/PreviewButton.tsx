543aa11f886ed2d49520d1d5e7865dc63e6bfd2c
ericodess <erickfcaldeira@gmail.com>
2022-01-10 17:32:06 -0300

    Implemented content component to the PreviewButton

--- src/components/PreviewButton.tsx
+++ src/components/PreviewButton.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const PreviewRedirector = () => {
-	return <Link to='/' className='preview-redirector'></Link>;
+//Utils
+import { ComponentAsProp } from '../utils/types';
+import { extractPropComponent } from '../utils/services';
+
+export interface PreviewRedirectorProps {
+	ContentComponent?: ComponentAsProp;
+	onHover?: React.MouseEventHandler<HTMLAnchorElement>;
+}
+
+const PreviewRedirector = ({ ContentComponent, onHover }: PreviewRedirectorProps) => {
+	return (
+		<Link to='/' className='preview-redirector' onMouseEnter={onHover}>
+			<div className='preview-redirector__film --opacity-ease-in' />
+			{extractPropComponent(ContentComponent)}
+		</Link>
+	);
 };
 
 export default PreviewRedirector;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,11 +4,20 @@ import React from 'react';
 import { Divisor, PreviewButton } from '../components';
 
 const Home = () => {
+	const getPreviewComponent = (previewURL: string) => {
+		return (
+			<img
+				src={previewURL}
+				style={{ width: '100%', height: '100%', objectFit: 'cover', objectPosition: '100%' }}
+			/>
+		);
+	};
+
 	return (
 		<div className='home --flex-center --zoom-in'>
-			<PreviewButton />
-			<Divisor />
-			<PreviewButton />
+			<PreviewButton ContentComponent={() => getPreviewComponent('https://wallpapercave.com/wp/wp2729921.gif')} />
+			<Divisor backgroundColor='#404040' />
+			<PreviewButton ContentComponent={() => getPreviewComponent('https://wallpapercave.com/wp/wp2729921.gif')} />
 		</div>
 	);
 };
