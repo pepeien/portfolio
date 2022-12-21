53d45de9ca89eb84271836b651ef52617a327cc1
ericodesu <erickfcaldeira@gmail.com>
2022-12-21 14:40:46 -0300

    Introduced language button component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,10 +1,12 @@
 export { default as Button } from './Button';
 export { default as ContactForm } from './ContactForm';
-export type { default as Divisor, DivisorProps, DivisorOrientation } from './Divisor';
+export { default as Divisor } from './Divisor';
 export { default as Modal } from './Modal';
 export { default as Navbar } from './Navbar';
+export { default as LanguageButton } from './LanguageButton';
 export { default as RectButton } from './RectButton';
 export { default as Specialty } from './Specialty';
 
 // Types
+export type { DivisorProps, DivisorOrientation } from './Divisor';
 export type { SpecialtyProps } from './Specialty';
