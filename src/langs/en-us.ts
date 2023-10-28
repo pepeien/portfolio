e7b5ebf71ff983479e9cde280d75b088abf3a785
pepeien <erickfcaldeira@gmail.com>
2023-10-27 23:38:20 -0300

    Pivoted the page design

--- src/langs/en-us.ts
+++ src/langs/en-us.ts
@@ -5,31 +5,17 @@ export default {
     LANGUAGE: 'English (US)',
 
     LANGUAGE_ABBREVIATION: 'enUs',
+    LANGUAGE_LOCALE: 'en_US',
 
-    PLACE_HOLDER_STATUS_1: 'Offline',
-    PLACE_HOLDER_STATUS_2: ':(',
-
-    STATUS_TEXT_REASSURING: 'Head somewhere',
-    STATUS_TEXT_LOCAL_OPTION: 'safe',
-    STATUS_TEXT_OUTSIDE_OPTION: 'outside',
-
-    ABOUT_TITLE: 'About',
-    ABOUT_TEXT:
-        "My name is *-Erick-*.||I'm a *-web-* specialized *-full stack developer-*.||" +
-        '*-Building-* robust *-applications-* to *-users-* and *-developers-*.',
+    ABOUT_TITLE: 'hi',
+    ABOUT_TITLE_SECOND: 'my name is',
+    ABOUT_NAME: 'ERICK',
 
     SPECIALTIES_TITLE: 'Specialties',
-    SPECIALTIES_MAIN_SKILL: 'Languages / Tools',
-    SPECIALTIES_SUB_SKILL: 'Frameworks / Libs',
 
     PROJECTS_TITLE: 'Projects',
-    PROJECTS_CODE: 'Code',
-    PROJECTS_LIVE_DEMO: 'Live Demo',
 
     SPECIALTY_TOOLS: 'Tools',
 
     CONTACT_TITLE: 'Contact',
-    CONTACT_EMAIL: 'Email',
-    CONTACT_MESSAGE: 'Message',
-    CONTACT_SEND_BUTTON: 'Send',
 } as Lang;
--- src/langs/pt-br.ts
+++ src/langs/pt-br.ts
@@ -5,31 +5,17 @@ export default {
     LANGUAGE: 'Português (BR)',
 
     LANGUAGE_ABBREVIATION: 'ptBr',
+    LANGUAGE_LOCALE: 'pt_BR',
 
-    PLACE_HOLDER_STATUS_1: 'Fora do ar',
-    PLACE_HOLDER_STATUS_2: ':(',
-
-    STATUS_TEXT_REASSURING: 'Vá par um lugar',
-    STATUS_TEXT_LOCAL_OPTION: 'seguro',
-    STATUS_TEXT_OUTSIDE_OPTION: 'lá fora',
-
-    ABOUT_TITLE: 'Sobre',
-    ABOUT_TEXT:
-        'Meu nome é *-Erick-*.||Sou um *-programador full stack-* com especialização *-web-*.||' +
-        '*-Criando aplicações-* robustas para *-usuários-* e *-desenvolvedores-*.',
+    ABOUT_TITLE: 'olá',
+    ABOUT_TITLE_SECOND: 'meu nome é',
+    ABOUT_NAME: 'ERICK',
 
     SPECIALTIES_TITLE: 'Especialidades',
-    SPECIALTIES_MAIN_SKILL: 'Linguagens / Ferramentas',
-    SPECIALTIES_SUB_SKILL: 'Frameworks / Bibliotecas',
 
     PROJECTS_TITLE: 'Projetos',
-    PROJECTS_CODE: 'Código',
-    PROJECTS_LIVE_DEMO: 'Demonstração',
 
     SPECIALTY_TOOLS: 'Ferramentas',
 
     CONTACT_TITLE: 'Contato',
-    CONTACT_EMAIL: 'Email',
-    CONTACT_MESSAGE: 'Menssagem',
-    CONTACT_SEND_BUTTON: 'Enviar',
 } as Lang;
