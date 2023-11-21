f28686aea70570d2d5c2886abca207c77ac69b95
pepeien <erickfcaldeira@gmail.com>
2023-11-21 14:42:25 -0300

    Removed wasted metadata form the root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,8 @@
 import React from 'react';
 import type { Metadata } from 'next';
 
-// Components
-import { Navbar } from '@components';
-
 // Dictionary
-import {
-    getAlternates,
-    getCanonicalAlternate,
-    getClientLocales,
-    getDictionary,
-    getServerDefaultLocale,
-} from '@dictionary';
-
-// Services
-import { InternalServices } from '@utils/services';
+import { getDictionary, getServerDefaultLocale } from '@dictionary';
 
 // Icons
 import icons from '@utils/icons';
@@ -31,40 +19,11 @@ export async function generateMetadata(): Promise<Metadata> {
 
     const title = dictionary['HOME_PAGE_TITLE'];
     const description = dictionary['HOME_PAGE_DESCRIPTION'];
-    const bannerURL = new URL(`${InternalServices.getBLOB()}/images/thumbnail.png`);
-    const banner = {
-        url: bannerURL,
-        secureUrl: bannerURL,
-        alt: `${dictionary['HOME_PAGE_TITLE']} banner`,
-        width: 1920,
-        height: 1080,
-    };
 
     return {
-        metadataBase: InternalServices.getDeploymentURL(),
-        alternates: {
-            canonical: getCanonicalAlternate(),
-            languages: getAlternates(),
-        },
         title: title,
         description: description,
         icons: icons,
-        openGraph: {
-            title: title,
-            description: description,
-            type: 'website',
-            images: banner,
-        },
-        twitter: {
-            card: 'summary_large_image',
-            title: title,
-            description: description,
-            images: banner,
-            site: process.env.TWITTER_HANDLE ?? undefined,
-        },
-        other: {
-            'google-site-verification': 'mo9T2NOuRjVjb6aO-W2GVlGhX8Pb6PCVVtvSxJg7V-I',
-        },
     };
 }
 
