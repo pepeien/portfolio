5b6972b245eb966f940f412c9c3d1bf409d442f5
pepeien <erickfcaldeira@gmail.com>
2023-11-25 21:08:12 -0300

    Removed unused imports

--- src/app/utils/interfaces/index.ts
+++ src/app/utils/interfaces/index.ts
@@ -1,12 +1,6 @@
 export type { Blog } from './blog';
 export type { DeviceOrientation } from './device-orientation';
-export type {
-    JobDictionary,
-    BlogDictionary,
-    ProjectDictionary,
-    PersonalDictionary,
-    Dictionary,
-} from './dictionary';
+export type { Dictionary } from './dictionary';
 export type { Job } from './job';
 export type { Project, ProjectIdentity } from './project';
 export type { Redirector } from './redirector';
