40ee5bea0d5ab2de6d23589fb96f8f15fcd174e5
pepeien <erickfcaldeira@gmail.com>
2024-04-17 01:06:55 -0300

    Improved sitemap locale support

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -7,14 +7,65 @@ import { Blog } from '@utils/interfaces';
 import { BlogServices, InternalServices, StringServices } from '@utils/services';
 
 // Dictionary
-import { getServerDefaultLocale } from '@dictionary';
+import { getCanonical, getServerLocales } from '@dictionary';
+import { Languages } from 'next/dist/lib/metadata/types/alternative-urls-types';
+
+type SiteMap = {
+    url: string;
+    lastModified?: string | Date;
+    changeFrequency?: 'always' | 'hourly' | 'daily' | 'weekly' | 'monthly' | 'yearly' | 'never';
+    priority?: number;
+    alternates?: {
+        languages?: Languages<string>;
+    };
+};
 
 const now = new Date();
 
+export const getAlternates = (prefix = '', suffix = '', hasCanonical = false) => {
+    const result: { [key: string]: string } = {};
+
+    const canonical = getCanonical();
+    const serverLocales = getServerLocales();
+    const locales = hasCanonical
+        ? Object.keys(serverLocales)
+        : Object.keys(serverLocales).filter((alternate) => alternate !== canonical);
+
+    locales.forEach((key) => {
+        result[key] = StringServices.removeExtraSlashes(
+            `${prefix}/${serverLocales[key]}/${suffix}`,
+        );
+    });
+
+    return result;
+};
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-    const deploymentURL = `${StringServices.removeExtraSlashes(
+    const defaultURL = StringServices.removeExtraSlashes(
         InternalServices.getDeploymentURL().toString(),
-    )}/${getServerDefaultLocale()}`;
+    );
+
+    const result: MetadataRoute.Sitemap = [];
+
+    const rootAlternates = getAlternates(defaultURL, '', true);
+    const rootSitemap = {
+        url: defaultURL,
+        lastModified: now,
+        changeFrequency: 'yearly',
+        priority: 0.9,
+        alternates: {
+            languages: rootAlternates,
+        },
+    } as SiteMap;
+
+    result.push(rootSitemap);
+
+    Object.values(rootAlternates).forEach((locale) => {
+        result.push({
+            ...rootSitemap,
+            url: locale,
+        });
+    });
 
     const releasedBlogPosts = await fetch(`${InternalServices.getBLOB()}/blog/metadata.json`, {
         next: { revalidate: InternalServices.getFetchInterval() },
@@ -23,25 +74,29 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
         .then((_blog: Blog[]) => _blog.filter((_post) => _post.status === 'RELEASED'))
         .catch(() => [] as Blog[]);
 
-    const sitemap: MetadataRoute.Sitemap = [];
-
-    sitemap.push({
-        url: deploymentURL,
-        lastModified: now,
-        changeFrequency: 'yearly',
-        priority: 1,
-    });
-
     if (releasedBlogPosts.length > 0) {
         const latestPost = BlogServices.getLatestPost(releasedBlogPosts);
 
-        sitemap.push({
-            url: `${deploymentURL}/blog/${latestPost.id}`,
+        const blogAlternates = getAlternates(defaultURL, `blog/${latestPost.id}`, true);
+        const blogSitemap = {
+            url: `${defaultURL}/blog/${latestPost.id}`,
             lastModified: latestPost.date,
             changeFrequency: 'monthly',
-            priority: 1,
+            priority: 0.8,
+            alternates: {
+                languages: blogAlternates,
+            },
+        } as SiteMap;
+
+        result.push(blogSitemap);
+
+        Object.values(blogAlternates).forEach((locale) => {
+            result.push({
+                ...blogSitemap,
+                url: locale,
+            });
         });
     }
 
-    return sitemap;
+    return result;
 }
