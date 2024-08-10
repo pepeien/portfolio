18cf7cab59a9a11a4980a5afe3f2bac4fc50abc3
pepeien <erickfcaldeira@gmail.com>
2024-08-10 13:08:55 -0300

    Updated sitemap

--- src/app/[lang]/sitemap.ts
+++ src/app/[lang]/sitemap.ts
@@ -1,14 +1,12 @@
 import { MetadataRoute } from 'next';
+import { Languages } from 'next/dist/lib/metadata/types/alternative-urls-types';
+import { headers } from 'next/headers';
 
 // Types
 import { Blog } from '@utils/interfaces';
 
 // Services
 import { BlogServices, InternalServices, StringServices } from '@utils/services';
-
-// Dictionary
-import { getCanonical, getServerLocales } from '@dictionary';
-import { Languages } from 'next/dist/lib/metadata/types/alternative-urls-types';
 
 type SiteMap = {
     url: string;
@@ -22,49 +20,20 @@ type SiteMap = {
 
 const now = new Date();
 
-export const getAlternates = (prefix = '', suffix = '', hasCanonical = false) => {
-    const result: { [key: string]: string } = {};
-
-    const canonical = getCanonical();
-    const serverLocales = getServerLocales();
-    const locales = hasCanonical
-        ? Object.keys(serverLocales)
-        : Object.keys(serverLocales).filter((alternate) => alternate !== canonical);
-
-    locales.forEach((key) => {
-        result[key] = StringServices.removeExtraSlashes(
-            `${prefix}/${serverLocales[key]}/${suffix}`,
-        );
-    });
-
-    return result;
-};
-
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-    const defaultURL = StringServices.removeExtraSlashes(
-        InternalServices.getDeploymentURL().toString(),
-    );
+    const url = `${StringServices.removeExtraSlashes(
+        `${InternalServices.getDeploymentURL().toString()}`,
+    )}/${headers().get('locale')}`;
 
     const result: MetadataRoute.Sitemap = [];
 
-    const rootAlternates = getAlternates(defaultURL, '', true);
     const rootSitemap = {
-        url: defaultURL,
+        url: url,
         lastModified: now,
         changeFrequency: 'yearly',
-        alternates: {
-            languages: rootAlternates,
-        },
     } as SiteMap;
 
     result.push(rootSitemap);
-
-    Object.values(rootAlternates).forEach((locale) => {
-        result.push({
-            ...rootSitemap,
-            url: locale,
-        });
-    });
 
     const releasedBlogPosts = await fetch(`${InternalServices.getBLOB()}/blog/metadata.json`, {
         next: { revalidate: InternalServices.getFetchInterval() },
@@ -88,23 +57,12 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
                 return -1;
             })
             .forEach((post) => {
-                const blogAlternates = getAlternates(defaultURL, `blog/${post.id}`, true);
                 const blogSitemap = {
-                    url: `${defaultURL}/blog/${post.id}`,
+                    url: `${url}/blog/${post.id}`,
                     lastModified: post.date,
-                    alternates: {
-                        languages: blogAlternates,
-                    },
                 } as SiteMap;
 
                 result.push(blogSitemap);
-
-                Object.values(blogAlternates).forEach((locale) => {
-                    result.push({
-                        ...blogSitemap,
-                        url: locale,
-                    });
-                });
             });
     }
 
---
+++ src/app/sitemap.xml/route.ts
@@ -0,0 +1,46 @@
+// Dictionary
+import { getCanonical, getServerLocales } from '@dictionary';
+
+// Services
+import { StringServices, InternalServices } from '@utils/services';
+
+const now = new Date();
+
+export const getAlternates = (prefix = '', suffix = '', hasCanonical = false) => {
+    const result: { [key: string]: string } = {};
+
+    const canonical = getCanonical();
+    const serverLocales = getServerLocales();
+    const locales = hasCanonical
+        ? Object.keys(serverLocales)
+        : Object.keys(serverLocales).filter((alternate) => alternate !== canonical);
+
+    locales.forEach((key) => {
+        result[key] = StringServices.removeExtraSlashes(
+            `${prefix}/${serverLocales[key]}/${suffix}`,
+        );
+    });
+
+    return result;
+};
+
+export function GET() {
+    const url = StringServices.removeExtraSlashes(InternalServices.getDeploymentURL().toString());
+    const alternates = Object.values(getAlternates(url, '', true));
+
+    const xmlContent = alternates.map((alternate) => {
+        return `<sitemap><loc>${`${alternate}/sitemap.xml`}</loc><lastmod>${now.toISOString()}</lastmod></sitemap>`;
+    });
+
+    const xml =
+        `<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${xmlContent}</sitemapindex>`.replaceAll(
+            ',',
+            '',
+        );
+
+    return new Response(xml, {
+        headers: {
+            'Content-Type': 'application/xml',
+        },
+    });
+}
